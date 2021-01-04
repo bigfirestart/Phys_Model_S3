b1a099bc82e13b286354931653e57559877e1f88
Mark <bigfirestart@gmail.com>
2021-01-04 17:14:43 +0300

    feat(results) results of lab added

--- module2/main.py
+++ module2/main.py
@@ -2,19 +2,15 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import argrelextrema
-from scipy.interpolate import interp1d
-import pandas as pd
 
 img = Image.open("rings.png")
-
 rgb_img = img.convert('RGB')
 
+# getting pixels
 res = []
 for i in range(0, rgb_img.width):
     res.append(255-rgb_img.getpixel((i, 300))[1])
-
-
-res = res[0:550]
+res = res[100:550]
 
 # find min and max
 maxs = argrelextrema(np.array(res), np.greater)[0]
@@ -26,22 +22,31 @@ maxs = maxs[:min_len]
 mins = mins[:min_len]
 vis_list = []
 i=1
-while i < len(maxs)-1:
-    prev_max_v = (maxs[i] - mins[i-1]) / (maxs[i] + mins[i-1])
-    vis_list.append(prev_max_v)
-    next_max_v = (maxs[i] - mins[i]) / (maxs[i] + mins[i])
-    vis_list.append(next_max_v)
+while i < len(mins)-1:
+    if maxs[i] > mins[i]:
+        prev_max_v = (maxs[i] - mins[i]) / (maxs[i] + mins[i])
+        vis_list.append(prev_max_v)
+    if maxs[i+1] > mins[i]:
+        next_max_v = (maxs[i+1] - mins[i]) / (maxs[i+1] + mins[i])
+        vis_list.append(next_max_v)
     i += 1
 
-# interpolation
-
-y = list(filter(lambda a: a > 0, vis_list))
+# approximation
+y = vis_list
 x = range(0, len(y))
 t = np.polyfit(x, y, 4)
 f = np.poly1d(t)
+
+# saving results
+res_coof = open("results/res_aprox_coof.txt", 'w')
+res_coof.write(str(f))
+
 plt.grid()
+plt.title("V(r)")
+plt.xlabel("r")
+plt.ylabel("V")
 plt.plot(x, y, 'o', x, f(x), 'b')
-plt.show()
+plt.savefig("results/res_plot.png")
 
 
 
